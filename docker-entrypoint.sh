e081f9b4c03df325e7aeb3f6a686e82eeb465f53
Adam Zammit <adam.zammit@acspri.org.au>
2020-06-05 10:40:53 +1000

    Allow entrypoint to continue even if cert download fails

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -62,7 +62,11 @@ EOPHP
 	# Install BaltimoreCyberTrustRoot.crt.pem
 	if [ ! -e BaltimoreCyberTrustRoot.crt.pem ]; then
 		echo "Downloading BaltimoreCyberTrustroot.crt.pem"
-		curl -o BaltimoreCyberTrustRoot.crt.pem -fsL "https://www.digicert.com/CACerts/BaltimoreCyberTrustRoot.crt.pem"
+		if curl -o BaltimoreCyberTrustRoot.crt.pem -fsL "https://www.digicert.com/CACerts/BaltimoreCyberTrustRoot.crt.pem"; then
+            echo "Downloaded successfully"
+        else
+            echo "Failed to download certificate - continuing anyway"
+        fi
     fi
 
     # see http://stackoverflow.com/a/2705678/433558
