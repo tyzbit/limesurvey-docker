183d62b1c564db00ff69ccdc9b9a84e96dd300f6
Adam Zammit <adam.zammit@acspri.org.au>
2020-06-05 10:41:30 +1000

    Fix: https://github.com/adamzammit/limesurvey-docker/issues/9

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -eu
 
+cd /var/www/html
+
 # usage: file_env VAR [DEFAULT]
 #    ie: file_env 'XYZ_DB_PASSWORD' 'example'
 # (will allow for "$XYZ_DB_PASSWORD_FILE" to fill in the value of
