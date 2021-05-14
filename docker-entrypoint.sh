1ad44b94fa07c2b2295bc320f5e20387a83ad5c1
Adam Zammit <adam.zammit@acspri.org.au>
2021-05-14 10:33:14 +1000

    Flush LimeSurvey cache/assets on restarting the container.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -174,6 +174,8 @@ EOPHP
         php application/commands/console.php resetpassword "$LIMESURVEY_ADMIN_USER" "$LIMESURVEY_ADMIN_PASSWORD"
 	fi
 
+    #flush asssets (clear cache on restart)
+    php application/commands/console.php flushassets
 fi
 
 exec "$@"
